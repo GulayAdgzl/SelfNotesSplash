a98ccb93d044a08d687625e236bc4b9307d474ac
Gülay ADIGÜZEL <44726684+GulayAdgzl@users.noreply.github.com>
2022-11-17 23:07:03 +0300

    Update splash.dart

--- splash/lib/splash/view/splash.dart
+++ splash/lib/splash/view/splash.dart
@@ -1,5 +1,9 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:splash/splash/view/splash_view.dart';
+
+import 'ButtonView.dart';
 
 class StartPage extends StatefulWidget {
   const StartPage({super.key});
@@ -12,19 +16,23 @@ class _StartPageState extends State<StartPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.arrow_forward),
-        onPressed: () {},
-      ),
-      body: Column(children: [
+
+        /*floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.arrow_forward),
+          onPressed: () {},
+        ),*/
+        body: Column(
+      children: [
         Spacer(flex: 2),
         Expanded(
           flex: 4,
           child: Splash(
             path: 'note',
           ),
-        )
-      ]),
-    );
+        ),
+        Spacer(flex: 2),
+        Expanded(flex: 4, child: ButtonView()),
+      ],
+    ));
   }
 }
